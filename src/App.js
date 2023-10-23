9593a98fb573e74932196506c01c3a57905cdb43
Jonatan <jonatangoncalves598@gmail.com>
2023-10-23 16:55:29 -0300

    Alteracoes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
-import './Estilos.css';
+import './App.css';
 function App() {
   const [contador1, setContador1] = useState(0);
   const [contador2, setContador2] = useState(0);
   const [soma, setSoma] = useState(0);
+  const [mulplicacao, setMultiplicacao] = useState(0)
 
-  
+
   return (
-    <div>
-      <h1 className="titulo">Componente Com Estilo</h1>
-      <input type="text" value={contador1} />
-      <input type="text" value={contador2} />
-      <input type="text" value={soma} />
-      <button onClick={() => setContador1(contador1 + 1)}>Clique aqui</button>
-      <button onClick={() => setContador2(contador2 + 1)}>Clique aqui</button>
-      <button onClick={() => setSoma(contador1 + contador2)}>Somar</button>
+    <div className="App">
+      <h1 className="titulo">Meu primeiro App em React</h1>
 
-      <p>Contagem 1: {contador1}</p>
-      <p>Contagem 2: {contador2}</p>
+      <p>Contador 1: {contador1}</p>
+      <p>Contador 2: {contador2}</p>
       <p>Soma: {soma}</p>
+      <p>Mulplicação: {mulplicacao}</p>
+      
+      <input type="text" value={contador1} onChange={() => { }} />
+      <input type="text" value={contador2} onChange={() => { }} />
+      <input type="text" value={soma} onChange={() => { }} />
+      <input type="text" value={mulplicacao} onChange={() => { }} />
+      <button onClick={() => setContador1(contador1 + 1)}> Clique aqui</button>
+      <button onClick={() => setContador2(contador2 + 1)}> Clique aqui</button>
+      <button onClick={() => setSoma(contador1 + contador2)}>Somar</button>
+      <button onClick={() => setMultiplicacao(contador1 * contador2 * soma)}>Mulplicação</button>
+
+
     </div>
   );
 }
